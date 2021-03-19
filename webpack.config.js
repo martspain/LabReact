5b5b4eb83479409bd07845a3baf9e9fca6a9e562
Martín España <46570760+martspain@users.noreply.github.com>
2021-03-19 17:55:28 -0600

    Aplicación inicial
    Commit inicial de Aplicación en React con algunas dependencias configuradas. Muestra solamente un "Hola mundo"

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    entry: './src/index.js',
+    output:{
+        filename: "name.bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    }
+
+}
